e54cbe4749767d50c87eb6ca556b83ed790bd239
harsh johar <hpsj3112@gmail.com>
2022-11-23 12:24:55 +0530

    uff this refactoring was tough!

--- lib/features/money/screens/details_screen.dart
+++ lib/features/money/screens/details_screen.dart
@@ -1,13 +1,8 @@
-import 'package:bigbucks/common/screens/loader.dart';
-import 'package:bigbucks/features/money/controller/money_controller.dart';
-import 'package:bigbucks/features/money/widgets/reciever_card.dart';
-import 'package:bigbucks/features/money/widgets/sender_card.dart';
-import 'package:bigbucks/models/transaction.dart';
+import 'package:bigbucks/features/money/widgets/transactions_list.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class DetailsScreen extends ConsumerStatefulWidget {
+class DetailsScreen extends ConsumerWidget {
   static const String routeName = '/details-screen';
   final String userId;
   final String name;
@@ -21,54 +16,13 @@ class DetailsScreen extends ConsumerStatefulWidget {
   }) : super(key: key);
 
   @override
-  ConsumerState<DetailsScreen> createState() => _DetailsScreenState();
-}
-
-class _DetailsScreenState extends ConsumerState<DetailsScreen> {
-  final ScrollController scrollController = ScrollController();
-
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.name),
+        title: Text(name),
       ),
-      body: StreamBuilder<List<TransactionModel>>(
-        stream: ref.read(moneyControllerProvider).getTransactions(widget.userId),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Loader();
-          }
-          if (snapshot.data == null || snapshot.data!.isEmpty) {
-            return const Center(child: Text("No Transactions"));
-          }
-          SchedulerBinding.instance.addPostFrameCallback((_) {
-            scrollController
-                .jumpTo(scrollController.position.maxScrollExtent);
-          });
-          return ListView.builder(
-            controller: scrollController,
-            shrinkWrap: true,
-            itemCount: snapshot.data!.length,
-            itemBuilder: (context, index) {
-              TransactionModel cardData = snapshot.data![index];
-              if (cardData.senderId == widget.userId) {
-                return RecieverCard(
-                  type: cardData.transactionType,
-                  amount: cardData.amount,
-                  description: cardData.description,
-                  timestamp: cardData.timestamp,
-                );
-              }
-              return SenderCard(
-                type: cardData.transactionType,
-                amount: cardData.amount,
-                description: cardData.description,
-                timestamp: cardData.timestamp,
-              );
-            },
-          );
-        },
+      body: TransactionList(
+        userId: userId,
       ),
     );
   }
