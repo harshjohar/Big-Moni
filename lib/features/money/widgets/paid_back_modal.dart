17f703d4e281cf434f1c479e58efbb9edfe97bcc
Nanak <nanakjotsachdeva@gmail.com>
2022-12-06 16:25:37 +0530

    modalsheet error message fixed

--- lib/features/money/widgets/paid_back_modal.dart
+++ lib/features/money/widgets/paid_back_modal.dart
@@ -22,6 +22,8 @@ class PaidBackModal extends ConsumerStatefulWidget {
 
 class _PaidBackModalState extends ConsumerState<PaidBackModal> {
   final TextEditingController amountController = TextEditingController();
+  final _amountFocusNode = FocusNode();
+  final _formKey = GlobalKey<FormState>();
 
   @override
   void dispose() {
@@ -30,15 +32,19 @@ class _PaidBackModalState extends ConsumerState<PaidBackModal> {
   }
 
   void _addTransaction() {
-    if (amountController.text.trim().isEmpty) {
-      showSnackBar(context: context, content: "Amount cannot be empty.");
+    final isValid = _formKey.currentState?.validate();
+    if (!(isValid as bool)) {
       return;
     }
+    //   if (amountController.text.trim().isEmpty) {
+    //     showSnackBar(context: context, content: "Amount cannot be empty.");
+    //     return;
+    //   }
 
-    if (double.parse(amountController.text.trim()) <= 0) {
-      showSnackBar(context: context, content: "Please enter a valid amount.");
-      return;
-    }
+    //   if (double.parse(amountController.text.trim()) <= 0) {
+    //     showSnackBar(context: context, content: "Please enter a valid amount.");
+    //     return;
+    //   }
     ref.read(moneyControllerProvider).payBackTransaction(
           context: context,
           userId: widget.userId,
@@ -58,36 +64,58 @@ class _PaidBackModalState extends ConsumerState<PaidBackModal> {
         20,
         MediaQuery.of(context).viewInsets.bottom + 10,
       ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Text(
-              "Paid Back by ${widget.name}",
-              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
+      child: Form(
+        key: _formKey,
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "Paid Back by ${widget.name}",
+                  style: const TextStyle(
+                      fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+              ),
+              TextFormField(
+                controller: amountController,
+                autofocus: true,
+                focusNode: _amountFocusNode,
+                onFieldSubmitted: (_) => _addTransaction(),
+                keyboardType: const TextInputType.numberWithOptions(
+                  decimal: true,
+                ),
+                validator: (value) {
+                  if (value!.trim().isEmpty) {
+                    FocusScope.of(context).requestFocus(_amountFocusNode);
+                    return "Amount cannot be empty";
+                  }
+                  if (double.tryParse(value.trim()) == null) {
+                    FocusScope.of(context).requestFocus(_amountFocusNode);
+                    return "Please enter a valid amount";
+                  }
+                  if (double.parse(value.trim()) <= 0) {
+                    FocusScope.of(context).requestFocus(_amountFocusNode);
+                    return "Please enter a valid amount";
+                  }
+                  return null;
+                },
+                decoration: const InputDecoration(
+                  hintText: "Amount",
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: CupertinoButton.filled(
+                  onPressed: _addTransaction,
+                  child: const Text("Submit"),
+                ),
+              ),
+            ],
           ),
-          TextField(
-            controller: amountController,
-            autofocus: true,
-            onSubmitted: (_) => _addTransaction(),
-            keyboardType: const TextInputType.numberWithOptions(
-              decimal: true,
-            ),
-            decoration: const InputDecoration(
-              hintText: "Amount",
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: CupertinoButton.filled(
-              onPressed: _addTransaction,
-              child: const Text("Submit"),
-            ),
-          ),
-        ],
+        ),
       ),
     );
   }
